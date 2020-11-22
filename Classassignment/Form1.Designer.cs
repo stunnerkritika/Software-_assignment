11b5ed3989b92cc28462ee3d4a1089402c64a97b
KritikaThapa <thapakritika019@gmail.com>
2020-11-22 20:15:23 +0545

    this program is completed ,fill,moveto,draw to and other validation remaining

---
+++ Classassignment/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Classassignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 247);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(396, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 299);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 53);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Command";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(562, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Result";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Classassignment/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classassignment
+{
+    public partial class Form1 : Form
+    {
+        Pen default_pen = new Pen(Color.Black); 
+        public void Submitted()
+
+        {
+            // MessageBox.Show("Title","Button_click");
+            switch (this.textBox1.Text)
+            {
+                case " ":
+                    {
+                        this.label4.Text = "please enter your code!!";
+                        break;
+                    }
+                default:
+                    {
+                        string command = this.textBox2.Text;
+                        command=command.ToLower().Trim();
+                        switch (command)
+                        {
+                            case "":
+                                {
+                                    this.label4.Text = "please enter your Command!!";
+                                    break;
+                                }
+                            case "run":
+                                {
+                                    this.label4.Text = " ";
+                                    Methodexecuting( this.textBox1.Text,this.textBox2.Text);
+
+                                    break;
+                                }
+                            case "clear":
+                                {
+                                    clear();
+                                    break;
+
+                                }
+                            case "reset":
+                                {
+                                    clear();
+                                    this.pictureBox1.Image = null;
+                                    break;
+
+                                }
+
+                            default:
+                                {
+                                    this.label4.Text = " Not a valid command!";
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+            }
+        }
+        public void clear()
+        {
+            this.label4.Text = " ";
+            this.textBox1.Text = " ";
+            this.textBox2.Text = " ";
+
+        }
+        public void DrawRectangle(string lengths, string breadths)
+        {
+            int length = int.Parse(lengths);
+            int breadth = int.Parse(breadths);
+            Graphics g = this.pictureBox1.CreateGraphics();
+            g.DrawRectangle(default_pen, 0, 0, breadth, length);
+        }
+        public void DrawCircle(string radiuss)
+        {
+            int radius = int.Parse(radiuss);
+            Graphics g = this.pictureBox1.CreateGraphics();
+            g.DrawEllipse(default_pen, 0, 0, radius * 2,radius*2);
+           
+        }
+       public void Drawtriangle(string side1,string side2,string side3)
+        {
+            int sideA = int.Parse(side1);
+            int sideB = int.Parse(side2);
+            int sideC = int.Parse(side3);
+            Graphics g = this.pictureBox1.CreateGraphics();
+            Point p1 = new Point(0,sideA);
+            Point p2 = new Point(sideA, sideB);
+            Point p3 = new Point(sideB, sideC);
+            Point[] points =
+            {
+                p1,p2,p3
+
+            };
+             g.DrawPolygon(default_pen,  points);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox1.Text = " ";
+            this.textBox2.Text = " ";
+        }
+        public void Methodexecuting(string code,string command)
+        {
+            code = code.ToLower().Trim();
+            string[] codearr = code.Split(' ');
+            switch (codearr[0])
+            {
+                case "rectangle":
+                    
+                    {
+                        if (codearr.Length == 3)
+                        {
+                            DrawRectangle(codearr[1], codearr[2]);
+                        }
+                        else
+                        {
+                            this.label4.Text = " There must be a length and a breadth";
+                        }
+                        break;
+                    }
+                case "circle":
+                    { 
+                        if(codearr.Length==2)
+                        {
+                            DrawCircle(codearr[1]);
+                        }
+                        else
+                        {
+                            this.label4.Text = " There must be  only one radius";
+
+                               
+                        }
+                        break;
+                    }
+                case "triangle":
+                    {
+                      if(codearr.Length==4)
+                        {
+                            Drawtriangle(codearr[1], codearr[2], codearr[3]);
+                          
+                        }
+                        else
+                        {
+                            this.label4.Text = "There must be three parameters";
+                        }
+                        break;
+
+                    }
+                    case "pen":
+                    {
+                        if(codearr.Length==2)
+                        {
+                         
+                        }
+                        else
+                        {
+                            this.label4.Text = "Must be color";
+                        }
+                        break;
+                    }
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Submitted();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
