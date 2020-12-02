9f3703171d20c0ac7aeeeca743a5c0cdde6c7cda
stunnerkritika <47621924+stunnerkritika@users.noreply.github.com>
2020-12-02 14:38:22 +0545

    Circle shape class implemented

---
+++ Classassignment/Interface1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Classassignment
+{
+    public  class Circle :Shapes
+    {
+        int radius;
+        Pen p;
+        SolidBrush  b;
+
+        public void getValue (int[] data , Pen pen)
+        {
+             radius = data[0];
+            p = pen;
+        }
+        public void getValue(int[] data, SolidBrush brush)
+        {
+            radius = data[0];
+            b = brush;
+        }
+
+
+    public void Draw (Graphics G , int x_axis , int y_axis, bool turnfill)
+        {
+            if (turnfill)
+            {
+                G.FillEllipse(b, x_axis, y_axis, radius, radius);
+            }
+            else
+            {
+                G.DrawEllipse(p, x_axis, y_axis, radius, radius);
+            }
+        }
+    }
+}
