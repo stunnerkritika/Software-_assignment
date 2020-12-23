7d48c65804fc2ebb5c3ae84c43c7f3d31d3ad18e
stunnerkritika <47621924+stunnerkritika@users.noreply.github.com>
2020-12-23 16:47:30 +0545

    Adding the temporary database to the system

---
+++ Classassignment/Data_config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections; 
+
+namespace Classassignment
+{
+    public class Data_config
+    {
+
+
+        //data storing in a array list for fetching it when ever necessary
+        public static ArrayList varaiablesList = new ArrayList();
+        public static ArrayList varaibleValue = new ArrayList();
+
+
+        //data storing in a array list for storing the method lines
+
+        public static ArrayList nameOfMethod = new ArrayList();
+        public static ArrayList lineOfCodeInMethod = new ArrayList();
+        public static ArrayList lengthOfMethod = new ArrayList();
+
+        public static ArrayList Method_parameters = new ArrayList();
+        public static ArrayList method_parameters_value = new ArrayList();
+        public static ArrayList index_of_parameter = new ArrayList();
+
+
+
+
+    }
+}
--- Classassignment/validation.cs
+++ Classassignment/validation.cs
@@ -7,40 +7,91 @@ using System.Windows.Forms;
 
 namespace Classassignment
 {
-   
+
     public class Validation
     {
         string errors = "";
-       /// <summary>
-       /// This construcor takes the code and command and vallidates it for further implementation
-       /// </summary>
-       /// <param name="code"></param>
-       /// <param name="command"></param>
-       public Validation(string code ,string command) {
+        string code = "";
+        /// <summary>
+        /// This construcor takes the code and command and vallidates it for further implementation
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="command"></param>
+        public Validation(string code)
+        {
+            this.code = code.Trim();
             switch (code)
             {
-               case " ":
+                case " ":
                     {
                         this.errors = "please enter your code!!";
                         break;
                     }
                 default:
                     {
-
-                        string[] code_array = code.Split(' ');
-                        if(code_array.Length == 2)
+                        if (code.Contains(";"))
                         {
-                            Code_analyzing ca = new Code_analyzing(code_array);
-                            this.errors = ca.error_handling_Code_analyzing();
+                            string[] code_array = code.Split(';');
+                            foreach (string codeBreaked in code_array)
+                            {
+                                if (codeBreaked != "")
+                                {
+                                    executing_the_code(codeBreaked.Trim());
+
+                                }
+
+
+                            }
                         }
                         else
                         {
-                            this.errors = "Please enter a valid code syntax";
+                            executing_the_code(code);
                         }
+
                         break;
+
                     }
             }
 
+        }
+
+        public void executing_the_code(string code)
+        {
+            string[] code_array = code.Split(' ');
+            if (code.Contains("method ") || (code.Contains("(") && code.Contains(")")))
+            {
+                Method_Implementation mi = new Method_Implementation(code);
+                this.errors = mi.error_handling_Method_implementation();
+            }
+            else if (code.Contains("loop for "))
+            {
+                Loop_Implementation li = new Loop_Implementation(code);
+                this.errors = li.error_handling_loop_implementation();
+            }
+            else if (code.Contains('=') || code.Contains('+') || code.Contains('%') || code.Contains('-') || code.Contains('/') || code.Contains('*') || code.Contains('<') || code.Contains('>'))
+            {
+                if (code.Contains("if "))
+                {
+                    Conditional_implementation ci = new Conditional_implementation(code);
+                    this.errors = ci.error_handling_conditional();
+                }
+                else
+                {
+
+                    Varaibles v = new Varaibles(code);
+                    this.errors = v.error_handling_varaibles();
+                }
+            }
+
+            else if (code_array.Length == 2)
+            {
+                Code_analyzing ca = new Code_analyzing(code_array);
+                this.errors = ca.error_handling_Code_analyzing();
+            }
+            else
+            {
+                this.errors = "Please enter a valid code syntax";
+            }
         }
 
         /// <summary>
